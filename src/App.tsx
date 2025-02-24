17728a34283da22247b9286c3b5995bde88af44e
Muhammad Mahad <mmahad913@gmail.com>
2025-02-24 19:20:37 +0500

    FFE3

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import ReactGA from 'react-ga4';
 import LocationService from './utils/LocationService';
 const TRACKING_ID = 'G-3TVZ4YD2FG';
 
+
 function App() {
   const location = useLocation();
   const persistedTheme = useSelector((state: any) => state.utils.theme);
